6264dc7cb1ccba315e5793bddbab5fe3edd0c5bf
trishateh <trishateh@gmail.com>
2024-02-29 17:21:47 +0800

    fix animation affect on page

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -16,24 +16,26 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br />
-            <TypeAnimation
-              sequence={[
-                "Trisha",
-                1000,
-                "Web3 Developer",
-                1000,
-                "Blockchain Developer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
+            <span className="inline-block h-[80px] md:h-[100px] lg:h-[180px] overflow-hidden align-top">
+              <TypeAnimation
+                sequence={[
+                  "Trisha",
+                  1000,
+                  "Web3 Developer",
+                  1000,
+                  "Blockchain Developer",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </span>
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Highly motivated software engineer with the ability to learn and
